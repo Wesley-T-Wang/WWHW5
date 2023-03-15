41a068dc3759d99168a7889a8404745e6639b194
Wesley Wang <Wesley.Wang.t@gmail.com>
2023-03-15 15:27:08 -0700

    324

--- buttoncount.js
+++ buttoncount.js
@@ -5,14 +5,54 @@ class ButtonCount extends HTMLElement {
         const template = document.createElement('template');
         template.innerHTML = `
         <style>
-            button {
-                background-color: #007BFF;
-                color: #fff;
-                padding: 8px 16px;
-                border-radius: 4px;
-            }
+        button {
+            appearance: none;
+            backface-visibility: hidden;
+            background-color: #27ae60;
+            border-radius: 8px;
+            border-style: none;
+            box-shadow: rgba(39, 174, 96, .15) 0 4px 9px;
+            box-sizing: border-box;
+            color: #fff;
+            cursor: pointer;
+            display: inline-block;
+            font-family: Inter,-apple-system,system-ui,"Segoe UI",Helvetica,Arial,sans-serif;
+            font-size: 16px;
+            font-weight: 600;
+            letter-spacing: normal;
+            line-height: 1.5;
+            outline: none;
+            overflow: hidden;
+            padding: 13px 20px;
+            position: relative;
+            text-align: center;
+            text-decoration: none;
+            transform: translate3d(0, 0, 0);
+            transition: all .3s;
+            user-select: none;
+            -webkit-user-select: none;
+            touch-action: manipulation;
+            vertical-align: top;
+            white-space: nowrap;
+          }
+          
+          button:hover {
+            background-color: #1e8449;
+            opacity: 1;
+            transform: translateY(0);
+            transition-duration: .35s;
+          }
+          
+          button:active {
+            transform: translateY(2px);
+            transition-duration: .35s;
+          }
+          
+          button:hover {
+            box-shadow: rgba(39, 174, 96, .2) 0 6px 12px;
+          }
         </style>
-        <button id="counter"><slot></slot></button>
+        <button id="counter">Times Clicked: 0</button>
         `;
         shadowRoot.appendChild(template.content.cloneNode(true));
         let count = 0;
